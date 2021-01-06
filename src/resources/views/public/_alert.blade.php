5e6990cead6e12ecde232860fd062b485c51dd34
Samuel De Backer <sdebacker@gmail.com>
2021-01-06 13:32:55 +0100

    alert close button

--- src/resources/views/public/_alert.blade.php
+++ src/resources/views/public/_alert.blade.php
@@ -1,8 +1,6 @@
 @if (Session::has('forum_alert'))
 <div class="alert alert-{{ Session::get('forum_alert_type') }} alert-dismissible fade show" role="alert">
-    <button type="button" class="close" data-bs-dismiss="alert" aria-label="Close">
-        <span aria-hidden="true">&times;</span>
-    </button>
     {{ Session::get('forum_alert') }}
+    <button class="btn-close" type="button" data-bs-dismiss="alert" aria-label="@lang('Close')"></button>
 </div>
 @endif
--- src/resources/views/public/_errors.blade.php
+++ src/resources/views/public/_errors.blade.php
@@ -1,15 +1,11 @@
 @if ($errors->count() > 0)
-<div class="alert alert-danger alert-dismissible fade show">
-    <div class="container position-relative">
-        <p><strong>@lang('Oh Snap!')</strong> @lang('Please fix the following errors:')</p>
-        <ul class="mb-0">
-            @foreach ($errors->all() as $error)
-            <li>{{ $error }}</li>
-            @endforeach
-        </ul>
-        <button type="button" class="close" data-bs-dismiss="alert" aria-label="Close">
-            <span aria-hidden="true">&times;</span>
-        </button>
-    </div>
+<div class="alert alert-danger alert-dismissible fade show" role="alert">
+    <p><strong>@lang('Oh Snap!')</strong> @lang('Please fix the following errors:')</p>
+    <ul class="mb-0">
+        @foreach ($errors->all() as $error)
+        <li>{{ $error }}</li>
+        @endforeach
+    </ul>
+    <button class="btn-close" type="button" data-bs-dismiss="alert" aria-label="@lang('Close')"></button>
 </div>
 @endif
